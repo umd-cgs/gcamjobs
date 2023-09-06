3fd9b0047cb747fb477ce55d175388c032be534e
Zarrar Khan <zarrar.khan@pnnl.gov>
2023-09-06 14:20:08 -0400

    Updating gcamjobs.R

---
+++ R/gcamjobs.R
@@ -0,0 +1,9 @@
+#' gcamjobs
+#'
+#' \itemize{
+#' \item Github: https://github.com/umd-cgs/gcamjobs
+#' \item Webpage: https://umd-cgs.github.io/gcamjobs/}
+#'
+#' @docType package
+#' @name gcamjobs
+NULL
--- R/rtemplate.R
+++
@@ -1,9 +0,0 @@
-#' rtemplate
-#'
-#' \itemize{
-#' \item Github: https://github.com/JGCRI/rtemplate
-#' \item Webpage: https://jgcri.github.io/rtemplate/}
-#'
-#' @docType package
-#' @name rtemplate
-NULL
