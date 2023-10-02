bf55eafdcf5b060c77943699b1885b5ea9f6139c
tzhao049 <tzhao049@gmail.com>
2023-10-02 14:45:45 -0400

    Testing scenarios

--- R/capacity.R
+++ R/capacity.R
@@ -5,6 +5,8 @@
 #' @param fig_dir Default = NULL, Output folder for figures and maps.
 #' @param prj_data Default = NULL, Gcam output data in "dat" format.
 #' @param input_dir Default = getwd(),output direction
+#' @param scenarios Default = NULL
+#' @param ref_scenario Default = NULL
 #' @keywords sum
 #' @return number
 #' @export
@@ -16,7 +18,9 @@
 
 capacity <- function(fig_dir = NULL,
                      prj_data = NULL,
-                     input_dir = getwd()) {
+                     input_dir = getwd(),
+                     scenarios  = NULL,
+                     ref_scenario  = NULL) {
   #.........................
   # Initialize
   #.........................
@@ -31,13 +35,80 @@ capacity <- function(fig_dir = NULL,
 
   rlang::inform("reading prj...")
   prj <- rgcam::loadProject(paste0(input_dir, "/", prj_data))
-  scenarios <- rgcam::listScenarios (prj)
+  all_scenarios <- rgcam::listScenarios (prj)
   queries = rgcam::listQueries(prj)
+
+  #.........................
+  # Test scenarios
+  #.........................
+  if(!all(scenarios %in% all_scenarios)){
+    rlang::warn("Not all the scenarios selected are in the database selected.")
+  }
+
+  #.........................
+  # Initial data
+  #.........................
+
+  # # process IRENA data
+  # IRENA_capacity %>%
+  #   select(Country, ISO.Code, Group.Technology, Technology, Poducer.Type, Year, Electricity.Installed.Capacity..MW.) %>%
+  #   rename(MW = "Electricity.Installed.Capacity..MW.") %>%
+  #   filter(Group.Technology %in% c("Solar energy", "Wind energy"), Year == 2020) %>%
+  #   mutate(ISO.Code = tolower(ISO.Code),
+  #          ISO.Code = gsub("rou", "rom", ISO.Code),
+  #          ISO.Code = gsub("xkx", "scg", ISO.Code),
+  #          ISO.Code = gsub("bes", "ant", ISO.Code),
+  #          value = MW,
+  #          value =as.numeric(gsub(",", "", value))) %>%
+  #   rename(year = Year) %>%
+  #   left_join(gcam_region_id, by = c("ISO.Code" = "iso")) %>%
+  #   left_join(gcam_region_name, by = "GCAM_region_ID") %>%
+  #   filter(!is.na(region)) -> IRENA_capacity_clean
+  #
+  #
+  # IRENA_capacity_clean %>%
+  #   left_join(IRENA.mapping %>%
+  #               select(-technology))%>%
+  #   group_by(region, year, var) %>%
+  #   summarise(value = sum(value, na.rm = T) * MW_to_GW) %>%
+  #   ungroup() %>%
+  #   mutate(scenario = ref_scenario) %>%
+  #   complete(nesting(region, year, var, value),
+  #            scenario = unique(scenarios),
+  #            fill = list(value =0)) %>%
+  #   select(scenario, year, value, region, var) -> IRENA_capacity_var
+  #
+  #
+  # IRENA_capacity_clean %>%
+  #   left_join(IRENA.tech.mapping) %>%
+  #   filter(!technology %in% c("rooftop_pv", "wind_offshore")) %>%
+  #   select(region, technology, year, value) %>%
+  #   # scale RE values to RE:RE storage ratio
+  #   left_join(RE_ratio_2020) %>%
+  #   mutate(tech_storage = ifelse(technology=="PV", value * PV_ratio, NA),
+  #          tech_storage = ifelse(technology=="CSP", value * CSP_ratio, tech_storage),
+  #          tech_storage = ifelse(technology=="wind", value * Wind_ratio, tech_storage),
+  #          technology = paste0(technology, "_storage"),
+  #          value = tech_storage) %>%
+  #   select(-CSP_ratio, -PV_ratio, -Wind_ratio, -tech_storage) %>%
+  #   # add original IRENA data
+  #   bind_rows(IRENA_capacity_clean %>%
+  #               left_join(IRENA.tech.mapping) %>%
+  #               select(region, technology, year, value)) %>%
+  #   group_by(region, year, technology) %>%
+  #   summarise(value = sum(value, na.rm = T) * MW_to_GW) %>%
+  #   ungroup() %>%
+  #   mutate(scenario = ref_scenario) %>%
+  #   complete(nesting(region, year, technology, value),
+  #            scenario = unique(scenarios),
+  #            fill = list(value =0)) -> IRENA_capacity_tech
+
+
 
   #.........................
   # Saving outputs
   #.........................
-  output = list(scenarios=scenarios,
+  output = list(all_scenarios = all_scenarios,
                 queries = queries)
 
 
@@ -51,3 +122,4 @@ capacity <- function(fig_dir = NULL,
   return(output)
 
 }
+
--- extras/Devtests.R
+++ extras/Devtests.R
@@ -1,11 +1,19 @@
 library(gcamjobs)
 library(rgcam)
 #testing funcation capacity
-input_dir_i = "/Users/Zt/Desktop/CGS_BRI/output/"
+input_dir_i = "//Users/Zt/Desktop/CGS_BRI/Repo/output/"
 prj_data_i = "GCAM_1.5_12_12.dat"
+scenarios_i <- c("BRI_1.5c_223230_gcam54_pricelink", "BRI_1.5c_2050_fullbio_pricelink","BRI_1.5c_2050_noLUC_fullbio_pricelink2","BRI_1.5c_2050_fullLUC_limbio_pricelink","BRI_1.5c_2050_fullLUC_fullbio_pricelink",
+                   "BRI_1.5c_2050_CO2BLDTRN_pricelink","BRI_1.5c_2050_AllRegions_pricelink","BRI_1.5c_2050_ctax_exp_sct","BRI_gcam54_cpol_updelec")
+
+ref_scenario_i <- "BRI_gcam54_cpol_updelec"
+
 gcamjobs::capacity(prj_data = prj_data_i,
-                   input_dir = input_dir_i)->
+                   input_dir = input_dir_i,
+                   scenarios  = scenarios_i,
+                   ref_scenario  = ref_scenario_i)->
   my_outputs
 
 my_outputs$scenarios
 my_outputs$queries
+
